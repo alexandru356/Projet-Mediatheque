2e46428fbb74910524ba4da68ad9a39406bb85f1
Andrei Cretu <andrei.cretu4676@gmail.com>
2024-11-19 13:06:15 -0500

    commit ids set

--- src/controller/DVDController.java
+++ src/controller/DVDController.java
@@ -1,5 +1,47 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import model.DVD;
+
 public class DVDController {
 
+	@FXML
+	private TableView<DVD> tableView;
+	
+	@FXML
+	private TableColumn<DVD, String> colNumDoc;
+	
+	@FXML
+	private TableColumn<DVD, String> colTitre;
+	
+	@FXML
+	private TableColumn<DVD, String> colRealisateur;
+	
+	@FXML
+	private TableColumn<DVD, String> colDate;
+	
+	@FXML
+	private TableColumn<DVD, Boolean> colEtat;
+	
+	@FXML
+	private TableColumn<DVD, Integer> colNbPret;
+	
+	@FXML
+	private TableColumn<DVD, String> colEmprunteur;
+	
+	private ObservableList<DVD> lstObsDVD = FXCollections.observableArrayList();
+	
+	public DVDController () {
+		
+	}
+	
+	@FXML
+	public void initialize () {
+		tableView.setItems(lstObsDVD);
+	}
+	
 }
--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module Mediatheque {
 	requires javafx.graphics;
 	requires javafx.fxml;
 	requires javafx.controls;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 //	opens controller to javafx.fxml;
