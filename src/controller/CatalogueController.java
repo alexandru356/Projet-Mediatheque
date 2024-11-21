b97df1434f2cf80a1c0784cc2325d3d6a70ca8a6
Andrei Cretu <andrei.cretu4676@gmail.com>
2024-11-21 13:45:41 -0500

    Refait catalogue Controller

--- src/controller/CatalogueController.java
+++ src/controller/CatalogueController.java
@@ -1,9 +1,43 @@
 package controller;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class CatalogueController {
 
+	@FXML
+	private RadioButton rdAuteur;
+	
+	@FXML
+	private RadioButton rdMotCle;
+	
+	@FXML
+	private Button btnQuitter;
+	
+	@FXML
+	private Button btnEffacer;
+	
+	@FXML
+	private TextField tfRecherche;
+	
+	public void Quitter () {
+		Platform.exit();
+	}
+	
+	public void Effacer () {
+		tfRecherche.clear();
+	}
+	
 	public CatalogueController () {
 		
 	}
 	
+	
+	@FXML
+	public void initialize () {
+		
+	}
 }
---
+++ src/utils/DVDReader.java
@@ -0,0 +1,13 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class DVDReader {
+
+	private DVDReader () {
+		throw new UnsupportedOperationException("Classe Utilitaire ne peut pas etre instancier");
+	}
+	
+}
