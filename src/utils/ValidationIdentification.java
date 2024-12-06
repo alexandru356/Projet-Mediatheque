ea8fc314bcc5f896244d44f1ca35261de956c813
Alexandru Ciuca <alexandru356.c@gmail.com>
2024-12-06 12:46:25 -0500

    admin log done

--- src/utils/ValidationIdentification.java
+++ src/utils/ValidationIdentification.java
@@ -1,7 +1,32 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class ValidationIdentification {
 
-	//Pour valider le nom et prenom ou le num de telephone lors de l'identification
+	public static boolean verifierId(String identifiant, String motDePasse) throws FileNotFoundException, IOException {
+		String pathFichierTexte = "/donnees/Compte.txt";
+		
+		try(InputStream inStream = ValidationIdentification.class.getResourceAsStream(pathFichierTexte);
+				BufferedReader reader = new BufferedReader(new InputStreamReader(inStream))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				String[] details = line.split(",");
+				if(details.length == 2) {
+					String loginID = details[0].trim();
+					String password = details[1].trim();
+					
+					if (loginID.equals(identifiant) && password.equals(motDePasse)) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
 	
 }
